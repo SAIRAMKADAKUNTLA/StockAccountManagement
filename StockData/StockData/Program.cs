59a9a865b36342556e1c6f87a6afe22c7d1f4295
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-12 20:11:19 +0530

    [Sairam]Added:inorder to get full portfolio of the stock account

---
+++ StockData/StockData/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StockData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to stock account management");
+            StockDetails stockDetails = new StockDetails();
+            stockDetails.GetStock();
+        }
+    }
+}
---
+++ StockData/StockData/StockDetails.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace StockData
+{
+    public class StockDetails
+    {
+        public void GetStock()
+        {
+            int valueOfStock ;
+            int totalValueOfStock = 0;
+            var json = File.ReadAllText(@"E:\BridgeLabz\Stock_Account_Management\StockAccountManagement\StockData\StockData\Stact.json");
+            var jObject = JObject.Parse(json);
+            var stockarray = (JArray)jObject["Stack_Account"];
+            if(jObject != null)
+            {
+                Console.WriteLine("--------Stock Data---------");
+                Console.WriteLine();
+                Console.WriteLine("StockName   NumberofShares   SharePrice   ValueofStock");
+                foreach(var iteam in stockarray)
+                {
+                    valueOfStock = TotalValue(iteam["NumOfShares"], iteam["SharePrice"]);
+                    totalValueOfStock += valueOfStock;
+                    Console.WriteLine(iteam["StockName"] + "    " + iteam["NumOfShares"] + "      " + iteam["SharePrice"] + "     " + valueOfStock);
+                    Console.WriteLine("total value"+ totalValueOfStock);
+                }
+            }
+        }
+        public int TotalValue(JToken NumOfShares, JToken SharePrice)
+        {
+            int shares = NumOfShares.ToObject<int>();
+            int price = SharePrice.ToObject<int>();
+            int valueOfStock = 0;
+
+            valueOfStock = shares * price;
+            
+            return valueOfStock;
+
+        }
+    }
+}
